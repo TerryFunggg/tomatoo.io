c82a01fd86cd270a793cba0d6217c3bea6f55faf
TerryFunggg <terryyessfung@gmail.com>
2021-05-18 23:27:10 +0800

    feat add file loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,28 +1,35 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-   entry: {
-     app: './src/index.js',
-   },
-   plugins: [],
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: "app.bundle.js"
+    path: path.resolve(__dirname, "public"),
+    filename: "app.bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.coffee$/,
-        loader: 'coffee-loader',
+        loader: "coffee-loader",
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [ "style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
-    ]
+      {
+        test: /\.(mp3|wav|mpe?g|ogg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+        },
+      },
+    ],
   },
 };
